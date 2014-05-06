e54e047d918eb38c4701f72f8b44f31a3e888f30
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-06 16:30:11 +0300

    Add Enqueue

--- msqueue_1.c
+++ msqueue_1.c
@@ -1,62 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct pointer_t{
+/*struct pointer_t{
 	struct node_t * ptr;
 	unsigned long long count;
 };
+*/
 
 struct node_t{
 	int value;//TODO: maybe change this
-	struct pointer_t next;
+	struct node_t * next;
 };
 
 struct queue_t{
-	struct pointer_t Head;
-	struct pointer_t Tail;
+	struct node_t * Head;
+	struct node_t * Tail;
 };
 
 
 void initialize(struct queue_t * Q){//TODO: init count?
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
-	node->next.ptr = NULL;
-	Q->Head.ptr = node; //TODO: check this
-	Q->Tail.ptr = node;
+	node->next = NULL;
+	Q->Head = node; //TODO: check this
+	Q->Tail = node;
 }
 
-int pointer_t_equal(struct pointer_t a , struct pointer_t b){
+/*int pointer_t_equal(struct pointer_t a , struct pointer_t b){
 	return ((a.ptr == b.ptr)&&(a.count == b.count));
 }
-
+*/
 void enqueue(struct queue_t * Q, int val){
-		
+	
+	struct node_t * tail;
 	int temp;	
-	struct pointer_t * arg =  (struct pointer_t *) malloc(sizeof(struct pointer_t)); //TODO: is this ok?
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
 	node->value = val;
-	node->next.ptr = NULL;
+	node->next = NULL;
 	while (1){
-		struct pointer_t tail = Q->Tail;
-		struct pointer_t next_p = (tail.ptr)->next;
-		if (pointer_t_equal(tail, Q->Tail)){
-			if (next_p.ptr == NULL){
-				arg->ptr = node;//TODO: is it ok?
-				arg->count = next_p.count + 1;
-				if (__sync_bool_compare_and_swap(&(tail.ptr->next),next_p,*arg))
+		tail = Q->Tail;
+		struct node_t * next_p = tail->next;
+		if (tail == Q->Tail){
+			if (next_p == NULL){
+				if (__sync_bool_compare_and_swap(&tail->next,next_p,node))
 					break;
 			}
 			else{
-				arg->ptr = next_p.ptr;
-				arg->count = tail.count + 1;
-				temp = __sync_bool_compare_and_swap(&Q->Tail,tail,arg);
+				temp = __sync_bool_compare_and_swap(&Q->Tail,tail,next_p);
 			}
 		}
 	}
-	arg->ptr = node;
-	arg->count = tail.count + 1;
-	temp = __sync_bool_compare_and_swap(&Q->Tail,tail,arg);			
+	temp = __sync_bool_compare_and_swap(&Q->Tail,tail,node);			
 }
 
+
+void printqueue(struct queue_t * Q){
+	
+	struct node_t * curr ;
+	struct node_t * next ;
+	
+	curr = Q->Head;
+	next = Q->Head->next;
+	while (curr != Q->Tail){
+		printf(" %d ",curr->value);
+		curr = next;
+		next = curr ->next;
+	}
+	printf("%d ",curr->value);
+	printf("\n");
+	
+}
 
 int main(int argc, char *argv[]){
 
@@ -64,13 +76,19 @@ int main(int argc, char *argv[]){
 
 	struct queue_t * Q = (struct queue_t *) malloc(sizeof(struct queue_t));
 	initialize(Q);
-	Q->Head.ptr->value = 5;
+	/*Q->Head.ptr->value = 5;
 	printf(" %d \n",Q->Head.ptr->value);
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
 	node->value = 6;
 	Q->Head.ptr->next.ptr=node;
 	printf(" %d \n",Q->Head.ptr->next.ptr->value);
-
+	*/
+	enqueue(Q,5);
+	enqueue(Q,2);
+	enqueue(Q,4);
+	enqueue(Q,5);
+	enqueue(Q,3);
+	printqueue(Q);
 	return 1;
 }
 	
