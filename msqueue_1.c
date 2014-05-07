daeadec68ca04aae94cc665852428bdfedf6dadf
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-07 15:33:26 +0300

    First, not working

--- msqueue_1.c
+++ msqueue_1.c
@@ -1,60 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct pointer_t{
-	struct node_t * ptr;
-	unsigned long long count;
-};
+
+
+
 
 struct node_t{
 	int value;//TODO: maybe change this
-	struct pointer_t next;
+	struct node_t * next;
 };
 
 struct queue_t{
-	struct pointer_t Head;
-	struct pointer_t Tail;
+	struct node_t * Head;
+	struct node_t * Tail;
+    int lock;
 };
 
 
 void initialize(struct queue_t * Q){//TODO: init count?
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
-	node->next.ptr = NULL;
-	Q->Head.ptr = node; //TODO: check this
-	Q->Tail.ptr = node;
+	node->next = NULL;
+	Q->Head = node; //TODO: check this
+	Q->Tail = node;
 }
 
-int pointer_t_equal(struct pointer_t a , struct pointer_t b){
-	return ((a.ptr == b.ptr)&&(a.count == b.count));
-}
 
 void enqueue(struct queue_t * Q, int val){
-		
-	int temp;	
-	struct pointer_t * arg =  (struct pointer_t *) malloc(sizeof(struct pointer_t)); //TODO: is this ok?
-	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
-	node->value = val;
-	node->next.ptr = NULL;
-	while (1){
-		struct pointer_t tail = Q->Tail;
-		struct pointer_t next_p = (tail.ptr)->next;
-		if (pointer_t_equal(tail, Q->Tail)){
-			if (next_p.ptr == NULL){
-				arg->ptr = node;//TODO: is it ok?
-				arg->count = next_p.count + 1;
-				if (__sync_bool_compare_and_swap(&(tail.ptr->next),next_p,*arg))
-					break;
-			}
-			else{
-				arg->ptr = next_p.ptr;
-				arg->count = tail.count + 1;
-				temp = __sync_bool_compare_and_swap(&Q->Tail,tail,arg);
-			}
-		}
-	}
-	arg->ptr = node;
-	arg->count = tail.count + 1;
-	temp = __sync_bool_compare_and_swap(&Q->Tail,tail,arg);			
+	
+    int store = 0;
+    struct node_t * tail = Q->Tail;
+    struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
+    node->value =  val;
+    node->next = NULL;
+    while(__sync_lock_test_and_set(&Q->lock,1));
+    tail=node;
+    __sync_lock_test_and_set(&Q->lock,0);
+
+}
+
+
+int dequeue(struct queue_t * Q, int * val){
+    
+}
+
+void printqueue(struct queue_t * Q){
+    
+    struct node_t * curr ;
+    struct node_t * next ;
+    
+    curr = Q->Head;
+    next = Q->Head->next;
+    while (curr != Q->Tail){
+        printf("%d ",curr->value);
+        curr = next;
+        next = curr ->next;
+    }
+    printf("%d ",curr->value);
+    printf("\n");
+    
 }
 
 
@@ -64,13 +67,16 @@ int main(int argc, char *argv[]){
 
 	struct queue_t * Q = (struct queue_t *) malloc(sizeof(struct queue_t));
 	initialize(Q);
-	Q->Head.ptr->value = 5;
+	/*Q->Head.ptr->value = 5;
 	printf(" %d \n",Q->Head.ptr->value);
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
 	node->value = 6;
 	Q->Head.ptr->next.ptr=node;
 	printf(" %d \n",Q->Head.ptr->next.ptr->value);
-
+    */
+    enqueue(Q,5);
+    enqueue(Q,7);
+    enqueue(Q,4);
 	return 1;
 }
 	
