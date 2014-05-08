638c8e4f9c8662c45bdb62768940dd139cf09750
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-08 13:27:23 +0300

    global lock first test

--- msqueue_1.c
+++ msqueue_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "timers_lib.h"
 
 
 
@@ -28,19 +28,37 @@ void initialize(struct queue_t * Q){//TODO: init count?
 void enqueue(struct queue_t * Q, int val){
 	
     int store = 0;
-    struct node_t * tail = Q->Tail;
     struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
     node->value =  val;
     node->next = NULL;
-    while(__sync_lock_test_and_set(&Q->lock,1));
-    tail=node;
-    __sync_lock_test_and_set(&Q->lock,0);
+    //while(__sync_lock_test_and_set(&Q->lock,1));
+    //#pragma omp critical
+    
+    struct node_t * tail = Q->Tail;
+    tail->next =node;
+    Q->Tail = node;
+    
+    //__sync_lock_test_and_set(&Q->lock,0);
 
 }
 
 
 int dequeue(struct queue_t * Q, int * val){
     
+    
+    struct node_t * node = Q->Head;
+    struct node_t * next = node->next;
+    //struct node_t * tail = Q->Tail;
+    if(next == NULL) return 0;
+    else{
+        *val = next->value;
+        Q->Head =next;
+    }
+    
+    free(node);
+    
+    return 1;
+           
 }
 
 void printqueue(struct queue_t * Q){
@@ -63,20 +81,40 @@ void printqueue(struct queue_t * Q){
 
 int main(int argc, char *argv[]){
 
-
+    int res,val,i,j; 
 
 	struct queue_t * Q = (struct queue_t *) malloc(sizeof(struct queue_t));
+    //Q->Head =  NULL;
+    //Q->Tail =  NULL;
 	initialize(Q);
-	/*Q->Head.ptr->value = 5;
-	printf(" %d \n",Q->Head.ptr->value);
-	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
-	node->value = 6;
-	Q->Head.ptr->next.ptr=node;
-	printf(" %d \n",Q->Head.ptr->next.ptr->value);
-    */
-    enqueue(Q,5);
+    /*enqueue(Q,5);
     enqueue(Q,7);
     enqueue(Q,4);
+    //res =  dequeue(Q,&val);
+    //if (res) printf("Dequeued %d \n",val);
+    enqueue(Q,1);
+    printqueue(Q)*/
+    
+    timer_tt * timer = timer_init();
+    timer_start(timer);
+    #pragma omp parallel for num_threads(4) shared(Q) private(res,val,i,j)
+    for(i=0;i<4;i++){
+        for(j=0; j<100000;j++){
+            #pragma omp critical
+            {
+                enqueue(Q,i);
+            }
+            #pragma omp critical
+            {    
+                res=dequeue(Q,&val);
+                //if(res) printf("%d ",val);
+            }
+        }
+    }
+    timer_stop(timer);
+    double timer_val = timer_report_sec(timer);
+    printf("total time %lf\n",timer_val);
+    printqueue(Q);
 	return 1;
 }
 	
