40224787b97d006992a75efa7281a28e16906e4a
Mpampis Stylianopoulos <mpastyl12@gmail.com>
2014-05-23 13:20:42 +0300

    fixed count overflow bug

--- msqueue_1.c
+++ msqueue_1.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "timers_lib.h"
-/*struct pointer_t{
-	struct node_t * ptr;
-	unsigned long long count;
-    unsigned long long both;
+struct pointer_t{
+    __int128 both;
 };
-*/
+
 
 
 
 struct node_t{
 	int value;//TODO: maybe change this
-	struct node_t *  next;
+	struct pointer_t  next;
 };
 
 struct queue_t{
-	struct node_t *  Head;
-	struct node_t *  Tail;
+	struct pointer_t  Head;
+	struct pointer_t  Tail;
 };
 
 
@@ -26,125 +24,126 @@ struct queue_t{
 	return ((a.ptr == b.ptr)&&(a.count == b.count));
 }
 */
+__int128 get_count(__int128 a){
 
-unsigned long long get_count(unsigned long long a){
-    
-    unsigned long long b = a >>48;
+    __int128 b = a >>64;
     return b;
 }
 
-unsigned long long get_pointer(unsigned long long a){
-    unsigned long long b = a << 16;
-    b= b >>16;
+__int128 get_pointer(__int128 a){
+    __int128 b = a << 64;
+    b= b >>64;
     return b;
 }
 
-unsigned long long set_count(unsigned long long  a, unsigned long long count){
-    unsigned long long count_temp =  count << 48;
-    unsigned long long b = get_pointer(a);
+__int128 set_count(__int128  a, __int128 count){
+    __int128 count_temp =  count << 64;
+    __int128 b = get_pointer(a);
     b = b | count_temp;
     return b;
 }
 
-unsigned long long set_pointer(unsigned long long a, unsigned long long ptr){
-    unsigned long long b = 0;
-    unsigned long long c = get_count(a);
+__int128 set_pointer(__int128 a, __int128 ptr){
+    __int128 b = 0;
+    __int128 c = get_count(a);
     b = set_count(b,c);
     ptr = get_pointer(ptr);
     b= b | ptr;
     return b;
 }
 
-unsigned long long set_both(unsigned long long a, unsigned long long ptr, unsigned long long count){
+__int128 set_both(__int128 a, __int128 ptr, __int128 count){
     a=set_pointer(a,ptr);
     a=set_count(a,count);
     return a;
 }
 
+
+
 void initialize(struct queue_t * Q){//TODO: init count?
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
-	node->next = NULL;
-	Q->Head= set_both(Q->Head,node,0); 
-	Q->Tail= set_both(Q->Tail,node,0);
+	node->next.both = 0;
+	Q->Head.both= set_both(Q->Head.both,node,0); 
+	Q->Tail.both= set_both(Q->Tail.both,node,0);
 }
 
 void enqueue(struct queue_t * Q, int val){
 	
-	struct node_t * tail;
-    struct node_t * new_to_set;
+	struct pointer_t tail;
+    __int128 new_to_set;
 	int temp;	
 	struct node_t * node = (struct node_t *) malloc(sizeof(struct node_t));
 	node->value = val;
-	node->next = NULL;
+	node->next.both = 0;
 	while (1){
 		tail = Q->Tail;
-		struct node_t * next_p = ((struct node_t *)get_pointer(tail))->next;
-		if (tail == Q->Tail){
-			if (next_p == NULL){
-                new_to_set =set_both(new_to_set,node,get_count(next_p) +1);
-				if (__sync_bool_compare_and_swap(&((struct node_t * )get_pointer(tail))->next,next_p,new_to_set))
+		struct pointer_t next_p = ((struct node_t *)get_pointer(tail.both))->next;
+		if (tail.both == Q->Tail.both){
+			if (get_pointer(next_p.both) == 0){
+                new_to_set =set_both(new_to_set,node,get_count(next_p.both) +(__int128)1);
+				if (__sync_bool_compare_and_swap(&(((struct node_t * )get_pointer(tail.both))->next.both),next_p.both,new_to_set))
 					break;
 			}
 			else{
-                new_to_set=set_both(new_to_set,next_p,get_count(tail)+1);
-				temp = __sync_bool_compare_and_swap(&Q->Tail,tail,new_to_set);
+                new_to_set=set_both(new_to_set,next_p.both,get_count(tail.both)+(__int128)1);
+				temp = __sync_bool_compare_and_swap(&Q->Tail.both,tail.both,new_to_set);
 			}
 		}
 	}
-    new_to_set=set_both(new_to_set,node,get_count(tail)+1);
-	temp = __sync_bool_compare_and_swap(&Q->Tail,tail,new_to_set);			
+    new_to_set=set_both(new_to_set,node,get_count(tail.both)+(__int128)1);
+	temp = __sync_bool_compare_and_swap(&Q->Tail.both,tail.both,new_to_set);
 }
 
 
 int dequeue(struct queue_t * Q,int * pvalue){
 
-	struct node_t * head;
-	struct node_t * tail;
-	struct node_t * next;
+	struct pointer_t head;
+	struct pointer_t tail;
+	struct pointer_t next;
 	int  temp;
 	int first_val;
-    struct node_t * new_to_set;
+    __int128 new_to_set;
 	while(1){
 		head =  Q->Head;
-        first_val=((struct node_t *)get_pointer(head))->value;
+        first_val=((struct node_t *)get_pointer(head.both))->value;
 		tail =  Q->Tail;
-		next =  ((struct node_t *)get_pointer(head))->next;
-		if ( head == Q->Head){
-			if (head == tail){
-				if ( next == NULL) 
+		next =  ((struct node_t *)get_pointer(head.both))->next;
+		if ( head.both == Q->Head.both){
+			if (head.both == tail.both){
+				if ( get_pointer(next.both) == 0) 
 					return 0;
-                new_to_set =  set_both(new_to_set,next,get_count(tail) +1);
-				temp = __sync_bool_compare_and_swap(&Q->Tail,tail,new_to_set);
+                new_to_set =  set_both(new_to_set,next.both,get_count(tail.both) +(__int128)1);
+				temp = __sync_bool_compare_and_swap(&Q->Tail.both,tail.both,new_to_set);
 			}
 			else{
                 //if ((head==Q->Head) &&(first_val!=((struct node_t *)get_pointer(head))->value)) printf("change detected!\n");
-				*pvalue =((struct node_t *)get_pointer(next))->value;
-                new_to_set = set_both(new_to_set,next,get_count(head)+1);
-				if( __sync_bool_compare_and_swap(&Q->Head,head,new_to_set))
+				*pvalue =((struct node_t *)get_pointer(next.both))->value;
+                new_to_set = set_both(new_to_set,next.both,get_count(head.both)+(__int128)1);
+				if( __sync_bool_compare_and_swap(&Q->Head.both,head.both,new_to_set))
 					break;
 			}
 		}
 	}
     //printf(" about to free %p \n",head);
-	free(get_pointer(head));
+	free(get_pointer(head.both));
 	return 1;
 }
 
 void printqueue(struct queue_t * Q){
 	
-	struct node_t * curr ;
-	struct node_t * next ;
+	struct pointer_t curr ;
+	struct pointer_t next ;
 	
 	curr = Q->Head;
     //printf(" in printqueue curr = %p\n",curr);
-	next = ((struct node_t * )get_pointer(Q->Head))->next;
+	next = ((struct node_t * )get_pointer(Q->Head.both))->next;
     //printf(" in printqueue next = %p\n",next);
-	while ((curr != Q->Tail)&&(curr!=NULL)){
+	while ((get_pointer(curr.both) != get_pointer(Q->Tail.both))&&(get_pointer(curr.both)!=0)){
     //printf(" in printqueue curr = %p\n",curr);
     //printf(" in printqueue next = %p\n",next);
-		printf("%d ",((struct node_t * )get_pointer(curr))->value);
+		printf("%d ",((struct node_t * )get_pointer(curr.both))->value);
 		curr = next;
-		if (next) next = ((struct node_t * )get_pointer(curr))->next;
+		if (get_pointer(next.both)) next = ((struct node_t * )get_pointer(curr.both))->next;
 	}
 	//printf("%d ",((struct node_t * )get_pointer(curr))->value);
 	printf("\n");
@@ -161,6 +160,7 @@ int main(int argc, char *argv[]){
     printf("%d %d \n",num_threads,count);
 	struct queue_t * Q = (struct queue_t *) malloc(sizeof(struct queue_t));
 	initialize(Q);
+
     //printf(" #### %p\n",&Q);
     //printf("count -> %llu\n",get_count(&Q));
     //printf("pointer -> %llu\n",get_pointer(&Q));
